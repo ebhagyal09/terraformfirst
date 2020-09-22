1cf20395530b9e7047fff327b294736a501306c6
root <root@ip-172-31-31-43.us-east-2.compute.internal>
2020-09-22 09:23:50 +0000

    added user data

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,63 +1,35 @@
-module "auto-scaling" {
-  source = "./modules/auto-scaling"
+## Creating Launch Configuration
 
-  region = "us-east-2"
+resource "aws_launch_configuration" "assignment_lc" {
+  image_id               = "${lookup(var.amis,var.region)}"
+  instance_type          = "t2.micro"
+  security_groups        = ["${aws_security_group.instance.id}"]
+  key_name               = "${var.key_name}"
+  user_data = <<-EOF
+              #!/bin/bash
+              wget https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.58/bin/apache-tomcat-8.5.58.tar.gz
+			  tar -zvxf apache-tomcat-9.0.10.tar.gz
+			  yum install wget -y
+			  chmod +x startup.sh
+              ./startup.sh
+			                
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+## Creating AutoScaling Group
 
-
-  #Launch Configuration
-
-  launch-configuration-name = "assignment-lc"
-  image-id = "ami-04fcd96153cb57194"
-  instance-type = "t2.micro"
-  launch-configuration-security-groups = "${aws_security_group.sg_22}"
-  
-  #Auto-Scaling
-
-  autoscaling-group-name = "assignment-asg"
-  max-size = "4"
-  min-size = "1"
-  health-check-grace-period = "300"
-  desired-capacity = "2"
-  force-delete = "true"
-  #A list of subnet IDs to launch resources in
-  vpc-zone-identifier = "${module.vpc.public-subnet-ids}"
-  target-group-arns = "arn:aws:elasticloadbalancing:eu-west-1:898668804275:targetgroup/cloudgeeks-tg/bbc376dff9e0dd2f"
-  health-check-type = "ALB"
-  tag-key = "Name"
-  tag-value = "testing"
-
-  #Auto-Scaling Policy-Scale-up
-  auto-scaling-policy-name-scale-up = "cpu-policy-scale-up"
-  adjustment-type-scale-up = "ChangeInCapacity"
-  scaling-adjustment-scale-up = "1"
-  cooldown-scale-up = "300"
-  policy-type-scale-up = "SimpleScaling"
-
-  #Auto-Scaling Policy Cloud-Watch Alarm-Scale-Up
-  alarm-name-scale-up = "cpu-alarm-scale-up"
-  comparison-operator-scale-up = "GreaterThanOrEqualToThreshold"
-  evaluation-periods-scale-up = "2"
-  metric-name-scale-up = "CPUUtilization"
-  namespace-scale-up = "AWS/EC2"
-  period-scale-up = "120"
-  statistic-scale-up = "Average"
-  threshold-scale-up = "70"
-
-  #Auto-Scaling Policy-Scale-down
-  auto-scaling-policy-name-scale-down = "cpu-policy-scale-down"
-  adjustment-type-scale-down = "ChangeInCapacity"
-  scaling-adjustment-scale-down = "-1"
-  cooldown-scale-down = "300"
-  policy-type-scale-down = "SimpleScaling"
-
-  #Auto-Scaling Policy Cloud-Watch Alarm-Scale-down
-  alarm-name-scale-down = "cpu-alarm-scale-down"
-  comparison-operator-scale-down = "LessThanOrEqualToThreshold"
-  evaluation-periods-scale-down = "2"
-  metric-name-scale-down = "CPUUtilization"
-  namespace-scale-down = "AWS/EC2"
-  period-scale-down = "120"
-  statistic-scale-down = "Average"
-  threshold-scale-down = "50"
-
+resource "aws_autoscaling_group" "terraform_asg" {
+  launch_configuration = "${aws_launch_configuration.terraform_asg.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 10
+  load_balancers = ["${aws_alb.terraform_asg.name}"]
+  health_check_type = "ALB"
+  tag {
+    key = "Name"
+    value = "terraform-asg-assignment"
+    propagate_at_launch = true
+  }
 }
